c4f462b82688bf6370776c22e12044269c86619a
taocv56 <chutao1993@gmail.com>
2013-03-25 22:15:53 -0700

    up test security

--- src/com/example/wifilocatordemo/WifiInfomation.java
+++ src/com/example/wifilocatordemo/WifiInfomation.java
@@ -8,13 +8,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.wifi.ScanResult;
-import android.net.wifi.WifiConfiguration;
-import android.net.wifi.WifiInfo;
+//import android.net.wifi.WifiConfiguration;
+//import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
+//import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -53,7 +53,7 @@ public class WifiInfomation extends Activity {
 						Toast.LENGTH_LONG).show();
 
 				if (view.getId() == R.id.buttonScan) {
-					
+					Log.d(TAG, "onClick() wifi.startScan()");
 					wifi.startScan();
 				}
 			}
@@ -90,7 +90,7 @@ public class WifiInfomation extends Activity {
 		  	 sb = new StringBuilder();
 		  	 List<ScanResult> results = wifi.getScanResults();
 		     for(int i = 0; i < results.size(); i++){
-		  		 sb.append(new Integer(i+1).toString() + ".");
+		  		 sb.append(Integer.valueOf(i + 1).toString() + ".");
 		  		 sb.append((results.get(i)).toString());
 		  		 sb.append("\n");
 		  	 }
--- src/com/example/wifilocatordemo/WifiSecurity.java
+++ src/com/example/wifilocatordemo/WifiSecurity.java
@@ -1,5 +1,6 @@
 package com.example.wifilocatordemo;
 
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -7,23 +8,26 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+
 //import android.widget.TextView;
 
-public class WifiActivity extends Activity {
-	private Button function1;
-	
+public class WifiSecurity extends Activity {
+	private Button btOK;
+	private EditText etPass;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.wifi_locator);
-		function1 = (Button) findViewById(R.id.function1);
-		
-		function1.setOnClickListener(new OnClickListener() {
+		setContentView(R.layout.security);
+		btOK = (Button) findViewById(R.id.btOK);
+		etPass = (EditText)findViewById(R.id.etPass);
+		btOK.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				startActivity(new Intent(WifiActivity.this,WifiInfomation.class));
+				if(etPass.getText().toString().compareTo("123abc")==0)
+					startActivity(new Intent(WifiSecurity.this,WifiActivity.class));
 			}
 		});
 	}
