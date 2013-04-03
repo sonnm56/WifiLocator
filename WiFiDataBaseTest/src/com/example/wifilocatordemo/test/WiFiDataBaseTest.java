8fddcab296bf81336e9ff652ba7cdde4378b4ecd
sonnm56 <nmson.khmt@gmail.com>
2013-04-03 20:32:27 +0700

    abc

--- WiFiDataBaseTest/src/com/example/wifilocatordemo/test/WiFiDataBaseTest.java
+++ WiFiDataBaseTest/src/com/example/wifilocatordemo/test/WiFiDataBaseTest.java
@@ -1,78 +1,35 @@
 package com.example.wifilocatordemo.test;
-import junit.framework.Assert;
-import android.test.*;
-import com.example.wifilocatordemo.TestPassAc;
+
 import com.example.wifilocatordemo.WiFiDataBase;
 
-import android.test.ActivityInstrumentationTestCase2;
-import android.test.suitebuilder.annotation.SmallTest;
-import android.widget.Button;
-import android.widget.EditText;
+import android.test.AndroidTestCase;
+import android.test.RenamingDelegatingContext;
 
-public class WiFiDataBaseTest extends ActivityInstrumentationTestCase2<WiFiDataBase> {
-	EditText HintText;
-	Button bt1;
-	Button btChange;
-	TestPassAc activity;
-	
+public class WiFiDataBaseTest extends AndroidTestCase {
+	  private static final String TEST_FILE_PREFIX = "test_";
+	  private WiFiDataBase mMyAdapter;
 
-	public WiFiDataBaseTest(Class<WiFiDataBase> name) {
-		super(name);
-		TestPassAc testPassAC = getActivity();
+	  @Override
+	  protected void setUp() throws Exception {
+	      super.setUp();
 
-		
-	}
-	public TestPassAcTest(){
-		super("com.example.wifilocatordemo", TestPassAc.class);
-		
-	}
-	protected void setUp() throws Exception {
-		super.setUp();
-		activity = getActivity();
-	    HintText = (EditText)activity.findViewById(com.example.wifilocatordemo.R.id.HintText);
-	    bt1 = (Button)activity.findViewById(com.example.wifilocatordemo.R.id.bt1);
-	    btChange = (Button)activity.findViewById(com.example.wifilocatordemo.R.id.bt1); 
-	}
-	protected void tearDown() throws Exception {
-	    super.tearDown();
-	  }
-	  
-	  @SmallTest
-	  public void testViewsCreated() {
-	    assertNotNull(getActivity());
-	    assertNotNull(HintText);
-	    assertNotNull(bt1);
-	    assertNotNull(btChange);
-	  }
-	  
-	  @SmallTest
-	  public void testViewsVisible() {
-	    ViewAsserts.assertOnScreen(HintText.getRootView(), bt1);
-	    ViewAsserts.assertOnScreen(bt1.getRootView(), HintText);
-	    ViewAsserts.assertOnScreen(btChange.getRootView(), HintText);
-	  }
-	  
-	  @SmallTest
-	  public void testStartingEmpty() {
-	    assertTrue("HintText field is empty", "".equals(HintText.getText().toString()));
-	    assertTrue("Test HintText", "Enter your password".equals(HintText.getHint().toString()));
-	    assertTrue("Text in Button", "OK".equals(bt1.getText().toString()));
-	    //assertTrue("Text in Button Change", "Change your password".equals(btChange.getText().toString()));
-	    
-	    
-	  }
-	  
-	  @SmallTest
-	  public void testKilosToPounds() {
-	    HintText.clearComposingText();
-	    bt1.clearComposingText();
-	    
-	    TouchUtils.tapView(this, HintText);
-	    sendKeys("123abc");
-	    
-	    assertTrue("1 kilo is 2.20462262 pounds", true);
-	  }
-	
+	      RenamingDelegatingContext context 
+	          = new RenamingDelegatingContext(getContext(), TEST_FILE_PREFIX);
 
-	
+	      mMyAdapter = new WiFiDataBase(context);
+	      mMyAdapter.open();
+	  }
+
+	  @Override
+	  protected void tearDown() throws Exception {
+	      super.tearDown();
+
+	      mMyAdapter.close();
+	      mMyAdapter = null;
+	  }
+
+	  public void testPreConditions() {
+	      assertNotNull(mMyAdapter);
+	  }
+
 }
--- WifiLocator/gen/com/example/wifilocatordemo/R.java
+++ WifiLocator/gen/com/example/wifilocatordemo/R.java
@@ -13,6 +13,8 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int idc=0x7f020001;
+        public static final int images=0x7f020002;
+        public static final int yz=0x7f020003;
     }
     public static final class id {
         public static final int HintText=0x7f070000;
@@ -20,14 +22,12 @@ public final class R {
         public static final int btChange=0x7f070002;
         public static final int btOK=0x7f070005;
         public static final int btOKChange=0x7f070004;
-        public static final int buttonScan=0x7f07000a;
+        public static final int buttonScan=0x7f070008;
         public static final int etChange=0x7f070003;
         public static final int etPass=0x7f070006;
         public static final int function1=0x7f070007;
-        public static final int function2=0x7f070009;
-        public static final int function3=0x7f070008;
-        public static final int menu_settings=0x7f07000c;
-        public static final int textStatus=0x7f07000b;
+        public static final int menu_settings=0x7f07000a;
+        public static final int textStatus=0x7f070009;
     }
     public static final class layout {
         public static final int pass=0x7f030000;
@@ -39,17 +39,16 @@ public final class R {
         public static final int wifiinfo=0x7f060000;
     }
     public static final class string {
-        public static final int Button1=0x7f040003;
-        public static final int Button2=0x7f040004;
-        public static final int Button3=0x7f040005;
-        public static final int Button4=0x7f040006;
-        public static final int ChangePassword=0x7f04000a;
-        public static final int HintText=0x7f040007;
+        public static final int Button1=0x7f040002;
+        public static final int Button2=0x7f040003;
+        public static final int Button3=0x7f040004;
+        public static final int Button4=0x7f040005;
+        public static final int ChangePassword=0x7f040009;
+        public static final int HintText=0x7f040006;
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int sScan=0x7f040008;
-        public static final int status=0x7f040009;
+        public static final int menu_settings=0x7f040001;
+        public static final int sScan=0x7f040007;
+        public static final int status=0x7f040008;
     }
     public static final class style {
         /** 
